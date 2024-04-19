a277643887e6346981368fa2eb98045c1deabab7
musmankhan12 <159324068+musmankhan12@users.noreply.github.com>
2024-04-19 10:05:08 -0700

    word counter

---
+++ app.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt({
+
+    name: "sentences",
+    type: "input",
+    message: "Enter your sentences to count words: "
+});
+
+let words = answer.sentences.trim().split(" ")
+
+console.log(words)
+
+console.log(`your sentences word count is ${words.length}`);
+``
